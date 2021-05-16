fdf4935437a1b56678a718c50fae51f19ffb2166
Anurag <anuragmisraphy99@gmail.com>
2021-05-16 22:09:12 +0530

    adding more features

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GlobalStyles } from "./global-styles";
+import 'normalize.css';
 
 import App from './App';
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
+    <GlobalStyles />
     <App />
-  </React.StrictMode>,
+  </>,
   document.getElementById('root')
 );
 
